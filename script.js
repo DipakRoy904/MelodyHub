afc73c8133168f6ad5fdd225471ad01ea810b23e
Dipak Roy <rdipak944@gmail.com>
2026-06-05 01:24:02 +0530

    Enhance song filtering and search functionality
    Added functionality for filtering songs by friend name and implemented a search bar to find songs by title or friend's name. Updated playlist rendering to accommodate new features.

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ let songs = [
     { filePath: "dipak_songs/Kaun Mera Special 26.mp3", coverPath: "https://images.unsplash.com/photo-1506157786151-b8491531f063?w=50&q=80", songTitle: "Kaun Mera", friendName: "Dipak" },
     { filePath: "dipak_songs/Hawayein.mp3", coverPath: "https://images.unsplash.com/photo-1478147427282-58a87a120781?w=50&q=80", songTitle: "Hawayein", friendName: "Dipak" },
     { filePath: "dipak_songs/Finding Her Kushagra.mp3", coverPath: "https://images.unsplash.com/photo-1478147427282-58a87a120781?w=50&q=80", songTitle: "Finding Her", friendName: "Dipak" },
-    { filePath: "dipak_songs/Khat Khat.mp3", coverPath: "https://images.unsplash.com/photo-1478147427282-58a87a120781?w=50&q=80", songTitle: "Khat Khat", friendName: "Dipak" },
+    { filePath: "dipak_songs/Khat Khat.mp3", coverPath: "https://images.unsplash.com/photo-1478147427282-58a87a120781?w=50&q=80", songTitle: "Khat_Khat", friendName: "Dipak" },
     { filePath: "dipak_songs/Zaalima.mp3", coverPath: "https://images.unsplash.com/photo-1506157786151-b8491531f063?w=50&q=80", songTitle: "Zaalima", friendName: "Dipak" }
 ];
 
